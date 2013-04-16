6906a4b0b9aec0ff6ab348d273a1614bfa4f7ca5
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-16 22:28:20 +0000

    add test cases

--- test/check.sh
+++ test/check.sh
@@ -1,6 +1,7 @@
 ./clean.sh
 for PROGRAM in base_test rdom_test split_reuse_inner_name_bug two_vector_args parallel \
-  vector_extern jit_stress parallel_alloc vector_bounds_inference parallel_nested
+  vector_extern jit_stress parallel_alloc vector_bounds_inference parallel_nested \
+  footprint
 do
   echo ${PROGRAM}
   python ../halide.py < ${PROGRAM}.halide > ${PROGRAM}.cpp
