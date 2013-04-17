7edd353b82fcc373cc274ddd520b0edae3a40b31
franciszw@gmail.com <franciszw@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-17 02:16:02 +0000

    debugging

--- trunk/test/check.sh
+++ trunk/test/check.sh
@@ -2,8 +2,8 @@
 for PROGRAM in base_test rdom_test split_reuse_inner_name_bug two_vector_args parallel \
   vector_extern jit_stress parallel_nested \
   fibonacci \
-  chunk_sharing mod chunk logical 
-#seg fault: #parallel_alloc vector_bounds_inference bounds_inference bounds_inference_chunk
+  chunk_sharing mod logical 
+#seg fault: #parallel_alloc vector_bounds_inference bounds_inference bounds_inference_chunk chunk
 do
   echo ${PROGRAM}
   python ../halide.py < ${PROGRAM}.halide > ${PROGRAM}.cpp
