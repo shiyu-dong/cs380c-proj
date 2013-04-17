a03d671e06c020a3754428f17732d6c6459b5176
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-17 00:16:14 +0000

    debugging rdom

--- test/check.sh
+++ test/check.sh
@@ -1,7 +1,7 @@
 ./clean.sh
 for PROGRAM in base_test rdom_test split_reuse_inner_name_bug two_vector_args parallel \
   vector_extern jit_stress parallel_alloc vector_bounds_inference parallel_nested \
-  footprint
+  bound_inference fibonacci
 do
   echo ${PROGRAM}
   python ../halide.py < ${PROGRAM}.halide > ${PROGRAM}.cpp
