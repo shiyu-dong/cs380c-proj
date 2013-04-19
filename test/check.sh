4bb73041293e3e8cd4fa53b50fe1084322947639
franciszw@gmail.com <franciszw@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-19 03:32:37 +0000

    testing side_effects and reduction_schedule

--- test/check.sh
+++ test/check.sh
@@ -2,8 +2,8 @@
 for PROGRAM in base_test split_reuse_inner_name_bug two_vector_args parallel \
   vector_extern jit_stress parallel_nested \
   fibonacci \
-  chunk_sharing mod logical c_function vector_cast vector_math
-#seg fault: #parallel_alloc vector_bounds_inference bounds_inference bounds_inference_chunk chunk split_store_compute
+  chunk_sharing mod logical c_function vector_cast vector_math parallel_alloc
+#seg fault: #vector_bounds_inference bounds_inference bounds_inference_chunk chunk split_store_compute
 #big memory: gpu_large_alloc
 #need new reduction implementation: reduction_schedule side_effects
 do
