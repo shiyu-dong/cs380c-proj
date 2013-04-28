57c9262c815ef7fd10c09ce2b319ee5f95f365f2
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-28 05:41:47 +0000

    add new test case wavelet.halide

--- trunk/test/check.sh
+++ trunk/test/check.sh
@@ -2,7 +2,8 @@
 for PROGRAM in base_test split_reuse_inner_name_bug parallel \
   vector_extern jit_stress parallel_nested \
   fibonacci \
-  chunk_sharing mod logical c_function parallel_alloc reduction_schedule
+  chunk_sharing mod logical c_function parallel_alloc reduction_schedule \
+  halide_blur lighten wavelet
 
 #use Func as Expr rather than image processing: #vector_bounds_inference bounds_inference bounds_inference_chunk chunk split_store_compute side_effects
 #big memory: gpu_large_alloc
