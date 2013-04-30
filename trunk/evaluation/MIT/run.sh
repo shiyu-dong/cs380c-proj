5afad6ce39c179a5bba1e0d010eb9b4be88fa6e6
dongshiyu1989@gmail.com <dongshiyu1989@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-30 21:41:24 +0000

    add bilateral_grid.cpp

--- trunk/evaluation/MIT/run.sh
+++ trunk/evaluation/MIT/run.sh
@@ -1,4 +1,5 @@
-for PROGRAM in blur test1 test2
+#for PROGRAM in blur test1 test2 reduction_schedule
+for PROGRAM in bilateral_grid
 do
   echo ${PROGRAM}
   g++ -std=c++0x -w ${PROGRAM}.cpp -L ../halide/ -lHalide -ldl -lpthread
