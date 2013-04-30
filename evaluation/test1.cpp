e0dddb263b39961fcf48b3932407b92ea7ec27f0
franciszw@gmail.com <franciszw@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-30 00:48:22 +0000

    vector C++ functions

--- evaluation/test1.cpp
+++ evaluation/test1.cpp
@@ -1,46 +1,39 @@
 #include "halide/Halide.h"
 using namespace Halide;
 
+#define size 1024
 int main(int argc, char **argv) {
   Var x, y, xi, yi;
-  const int size = 1024;
 
   //input
   Image<double> input(size, size);
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
-      input(j,i) = (double)rand() / RAND_MAX;
+      input(i,j) = (double)rand() / RAND_MAX;
     }
   }
 
-  Func blur_x, blur_y, blur_all;
+  Func blur_x, blur;
 
   ////The algorithm
   blur_x(x, y) = (input(x-1, y) + input(x, y) + input(x+1, y))/3;
-  blur_y(x, y) = (blur_x(x, y-1) + blur_x(x, y) + blur_x(x, y+1))/3;
-  blur_all(x, y) = (blur_y(x-1, y) + blur_y(x, y-1) + blur_y(x+1, y) + blur_y(x, y+1) + blur_y(x-1, y-1) + blur_y(x-1, y+1) + blur_y(x+1, y+1) + blur_y(x+1, y-1))/8;
+  blur(x, y) = (blur_x(x, y-1) + blur_x(x, y) + blur_x(x, y+1))/3;
 
   // The schedule
-  blur_x.tile(x, y, xi, yi, 128, 128);
-  blur_x.vectorize(xi, 8);
-  blur_x.parallel(x);
-
-  blur_y.tile(x, y, xi, yi, 128, 128);
-  blur_y.vectorize(yi, 8);
-  blur_y.parallel(y);
-
-  blur_all.tile(x, y, xi, yi, 128, 128);
-  blur_all.vectorize(yi, 8);
-  blur_all.parallel(y);
+  //blur_x.tile(x, y, xi, yi, 128, 128).vectorize(xi, 8).parallel(x);
+  //blur_y.tile(x, y, xi, yi, 128, 128).vectorize(yi, 8).parallel(y);
+  
+//  blur.tile(x, y, xi, yi, 1024, 1024);
 
   //output
-  Image<double> output = blur_all.realize(size, size);
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      printf("%f ", output(j, i));
-    }
-    printf("\n");
-  }
+  Image<double> output = blur.realize(1000, 1000);
+
+//  for (int i = 0; i < size; i++) {
+//    for (int j = 0; j < size; j++) {
+//      printf("%f ", output(i, j));
+//    }
+//    printf("\n");
+//  }
 
   return 0;
 }
