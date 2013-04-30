e6549cfecd7e96478fb125704b29294f883383f6
franciszw@gmail.com <franciszw@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-30 20:06:28 +0000

    working on evaluation

--- trunk/evaluation/test1.cpp
+++ trunk/evaluation/test1.cpp
@@ -1,39 +1,19 @@
 #include "halide/Halide.h"
 using namespace Halide;
 
-#define size 1024
 int main(int argc, char **argv) {
-  Var x, y, xi, yi;
+  Var x, y;
 
   //input
-  Image<double> input(size, size);
-  for (int i = 0; i < size; i++) {
-    for (int j = 0; j < size; j++) {
-      input(i,j) = (double)rand() / RAND_MAX;
-    }
-  }
-
-  Func blur_x, blur;
+  Func input;
+  Func avg;
 
   ////The algorithm
-  blur_x(x, y) = (input(x-1, y) + input(x, y) + input(x+1, y))/3;
-  blur(x, y) = (blur_x(x, y-1) + blur_x(x, y) + blur_x(x, y+1))/3;
-
-  // The schedule
-  //blur_x.tile(x, y, xi, yi, 128, 128).vectorize(xi, 8).parallel(x);
-  //blur_y.tile(x, y, xi, yi, 128, 128).vectorize(yi, 8).parallel(y);
-  
-//  blur.tile(x, y, xi, yi, 1024, 1024);
+  input(x, y) = cast<float>(y)*x;
+  avg(x, y) = input(x-1, y)+input(x+1,y);
 
   //output
-  Image<double> output = blur.realize(1000, 1000);
-
-//  for (int i = 0; i < size; i++) {
-//    for (int j = 0; j < size; j++) {
-//      printf("%f ", output(i, j));
-//    }
-//    printf("\n");
-//  }
+  Image<float> output = avg.realize(8, 8);
 
   return 0;
 }
