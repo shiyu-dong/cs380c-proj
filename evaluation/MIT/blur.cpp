249db2fef870f1fb42ee1bd753e08777caf43ce3
franciszw@gmail.com <franciszw@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-04-30 21:18:01 +0000

    debugged py, added test cases

--- evaluation/MIT/blur.cpp
+++ evaluation/MIT/blur.cpp
@@ -7,19 +7,26 @@ int main(int argc, char **argv) {
 
   Func input, blur_x, blur;
 
+  Image<double> in(size, size);
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      in(j,i) = (double)rand() / RAND_MAX;
+    }
+  }
+
   ////The algorithm
-  input(x, y) = cast<float>(x*y+0.2);
+  input(x, y) = in(clamp(x, 0, size-1), clamp(y, 0, size-1));
   blur_x(x, y) = (input(x-1, y) + input(x, y) + input(x+1, y))/3;
   blur(x, y) = (blur_x(x, y-1) + blur_x(x, y) + blur_x(x, y+1))/3;
 
   // The schedule
-  //blur_x.tile(x, y, xi, yi, 128, 128).vectorize(xi, 8).parallel(x);
-  //blur_y.tile(x, y, xi, yi, 128, 128).vectorize(yi, 8).parallel(y);
-  
-  blur.parallel(y);
+  input.root();
+
+  blur_x.tile(x, y, xi, yi, 128, 128).vectorize(xi, 8).parallel(y);
+  blur.tile(x, y, xi, yi, 128, 128).vectorize(yi, 8).parallel(y);
 
   //output
-  Image<float> output = blur.realize(size, size);
+  Image<double> output = blur.realize(size, size);
 
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
