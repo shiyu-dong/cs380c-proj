7883452f17f62b95cc23252f3d0e5fb03d2202f7
franciszw@gmail.com <franciszw@gmail.com@d1925048-b7a9-8be4-e848-07404b66c3e6>
2013-05-02 01:02:44 +0000

    local run done

--- trunk/evaluation/MIT/blur.cpp
+++ trunk/evaluation/MIT/blur.cpp
@@ -1,9 +1,12 @@
+#include <sys/time.h>
 #include "halide/Halide.h"
 using namespace Halide;
 
-#define size 2048
+#define size 8192
 int main(int argc, char **argv) {
   Var x, y, xi, yi;
+  struct timeval start;
+  struct timeval end;
 
   Func input, blur_x, blur;
 
@@ -14,6 +17,8 @@ int main(int argc, char **argv) {
     }
   }
 
+  gettimeofday(&start, NULL);
+
   ////The algorithm
   input(x, y) = in(clamp(x, 0, size-1), clamp(y, 0, size-1));
   blur_x(x, y) = (input(x-1, y) + input(x, y) + input(x+1, y))/3;
@@ -22,19 +27,17 @@ int main(int argc, char **argv) {
   // The schedule
   input.root();
 
-  blur_x.tile(x, y, xi, yi, 128, 128).vectorize(xi, 8).parallel(y);
-  blur.tile(x, y, xi, yi, 128, 128).vectorize(yi, 8).parallel(y);
+  blur_x.tile(x, y, xi, yi, 128, 128).parallel(y);
+  blur.tile(x, y, xi, yi, 128, 128).parallel(y);
 
   //output
   Image<double> output = blur.realize(size, size);
 
-  //for (int i = 0; i < size; i++) {
-  //  for (int j = 0; j < size; j++) {
-  //    printf("%f ", output(i, j));
-  //  }
-  //  printf("\n");
-  //}
-  printf("Success!\n");
+  gettimeofday(&end, NULL);
+
+  printf("time: %ld\n", (end.tv_sec - start.tv_sec)*1000000 + (end.tv_usec-start.tv_usec));
+
+  printf("Done!\n");
 
   return 0;
 }
